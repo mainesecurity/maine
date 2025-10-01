970be1ef10b20cf610da401a1d3716d6806fcef6
maine <mainesecurity>
2025-10-01 23:08:47 +0200

    Changed External Link from github to HackTheBox

--- src/config.ts
+++ src/config.ts
@@ -45,8 +45,8 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
-			url: "https://github.com/mainesecurity", // Internal links should not include the base path, as it is automatically added
+			name: "HackTheBox",
+			url: "https://app.hackthebox.com/profile/855535", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
